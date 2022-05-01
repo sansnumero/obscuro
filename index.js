d9bb70b82f2b4f88f0d14c3d4fa8794a1235b000
sansnumero <104723813+sansnumero@users.noreply.github.com>
2022-05-01 16:41:26 -0400

    Add comments

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const path = require('path');
 const knexConfig = require('./db/knexfile');
 const knex = require('knex')(knexConfig['development'])
 const comment_mapper = require('./services/comment_mapper');
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 let current_user = {
   id: 1000,
   username: "Darth Vader",
   picture_url: "https://image.shutterstock.com/image-photo/san-benedetto-del-tronto-italy-600w-239338216.jpg",
 }
+
+app.post("/comment/create",function(req,res){
+  knex('comments')
+  .insert({
+    content: req.body.comment_content, 
+    user_id: current_user.id,
+    upvote_count: 0,
+    created_at: Date.now(),
+    updated_at: Date.now()
+  })
+  .then((what) => {
+    console.log("what")
+    console.log(what)
+    res.redirect('/');
+  })
+});
 
 app.get('/', (req, res) => {
   knex('users')
