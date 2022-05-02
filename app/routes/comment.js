365f3738c45fc5bd7889811a48c66fafe9a917fe
sansnumero <104723813+sansnumero@users.noreply.github.com>
2022-05-01 20:14:14 -0400

    Extract routes

---
+++ app/routes/comment.js
@@ -0,0 +1,52 @@
+const knexConfig = require('../../db/knexfile');
+const knex = require('knex')(knexConfig['development'])
+const comment_mapper = require('../services/comment_mapper');
+
+function getComments(req, res) {
+	knex('users')
+  .innerJoin('comments', 'users.id', 'comments.user_id')
+  .select('users.username', 'users.picture_url', 'comments.created_at', 'comments.content', 'comments.upvote_count', 'comments.id')
+  .then((comments) => {
+    return res.render('index', {
+      comments: comments.map(comment_mapper.map)
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    return res.json({success: false, message: 'An error occurred, please try again later.'});
+  })
+}
+
+function updateComment(req, res) {
+	knex('comments')
+  .where('id', req.params.commentId)
+  .update({
+    updated_at: Date.now(),
+    upvote_count: knex.raw('upvote_count + 1')
+  })
+  .then(() => {
+    res.sendStatus(200);
+  });
+}
+
+function postComment(req, res) {
+	let current_user = {
+		id: 1000,
+		username: "Darth Vader",
+		picture_url: "https://image.shutterstock.com/image-photo/san-benedetto-del-tronto-italy-600w-239338216.jpg",
+	}
+
+	knex('comments')
+	.insert({
+		content: req.body.comment_content, 
+		user_id: current_user.id,
+		upvote_count: 0,
+		created_at: Date.now(),
+		updated_at: Date.now()
+	})
+	.then(() => {
+		res.redirect('/');
+	});
+}
+
+module.exports = { getComments, updateComment, postComment };
--- index.js
+++ index.js
@@ -1,62 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
-const knexConfig = require('./db/knexfile');
-const knex = require('knex')(knexConfig['development'])
-const comment_mapper = require('./services/comment_mapper');
 const bodyParser = require("body-parser");
+let comment = require('./app/routes/comment');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('views'));
 
-let current_user = {
-  id: 1000,
-  username: "Darth Vader",
-  picture_url: "https://image.shutterstock.com/image-photo/san-benedetto-del-tronto-italy-600w-239338216.jpg",
-}
+app.route("/comment/create")
+  .post(comment.postComment);
 
-app.post("/comment/create",function(req,res){
-  knex('comments')
-  .insert({
-    content: req.body.comment_content, 
-    user_id: current_user.id,
-    upvote_count: 0,
-    created_at: Date.now(),
-    updated_at: Date.now()
-  })
-  .then(() => {
-    res.redirect('/');
-  })
-});
+app.route("/comments/:commentId")
+  .put(comment.updateComment);
 
-app.put("/comments/:commentId",function(req,res){
-  knex('comments')
-  .where('id', req.params.commentId)
-  .update({
-    updated_at: Date.now(),
-    upvote_count: knex.raw('upvote_count + 1')
-  })
-  .then(() => {
-    res.sendStatus(200);
-  });
-});
-
-app.get('/', (req, res) => {
-  knex('users')
-  .innerJoin('comments', 'users.id', 'comments.user_id')
-  .select('users.username', 'users.picture_url', 'comments.created_at', 'comments.content', 'comments.upvote_count', 'comments.id')
-  .then((comments) => {
-    return res.render('index', {
-      comments: comments.map(comment_mapper.map)
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-    return res.json({success: false, message: 'An error occurred, please try again later.'});
-  })
-});
+app.route("/")
+  .get(comment.getComments);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
